382e48679dd57f654ee0cc0c0cdf02aa2b583111
meorionel <therese.sjdyka@gmail.com>
2026-01-06 15:05:01 +0800

    feat: fix bun path

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,13 @@
+const dotenv = require("dotenv");
+const { parsed } = dotenv.config({ path: ".env" });
+
 module.exports = {
 	apps: [
 		{
 			name: "CountElysia",
 			port: "4001",
 			script: "./src/index.ts",
-			interpreter: "/Users/aliceclodia/.bun/bin/bun",
+			interpreter: parsed?.BUN_BIN,
 			env_file: ".env",
 		},
 	],
